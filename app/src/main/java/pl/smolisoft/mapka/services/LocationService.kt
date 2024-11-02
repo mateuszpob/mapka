4181bccac46194a2c6dcd7bbc162223190ff6dd1
mateuszpob <mateuszpob@gmail.com>
2024-11-02 20:35:28 +0100

    LocationService v0.2

--- app/src/main/java/pl/smolisoft/mapka/services/LocationService.kt
+++ app/src/main/java/pl/smolisoft/mapka/services/LocationService.kt
@@ -6,8 +6,14 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
 import android.os.IBinder
 import android.os.Looper
 import android.util.Log
@@ -18,29 +24,37 @@ import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import pl.smolisoft.mapka.R
 
-class LocationService: Service() {
+class LocationService : Service() {
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationManager: LocationManager
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            Log.d("LocationService", "Location: ${location.latitude}, ${location.longitude}")
+            LocationRepository.updateLocation(location) // Aktualizacja lokalizacji w repozytorium
+        }
 
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            for (location in locationResult.locations) {
-                Log.d("LocationService", "Location: ${location.latitude}, ${location.longitude}")
-                LocationRepository.updateLocation(location)
-            }
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            // Obsługa zmiany statusu dostawcy
+        }
+
+        override fun onProviderEnabled(provider: String) {
+            Log.d("LocationService", "Provider enabled: $provider")
+        }
+
+        override fun onProviderDisabled(provider: String) {
+            Log.d("LocationService", "Provider disabled: $provider")
         }
     }
 
     override fun onCreate() {
-        Log.d("LocationService", "Start LocationService")
         super.onCreate()
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        locationRequest = LocationRequest.Builder(
-            300
-        ).build()
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        // Tworzenie powiadomienia dla serwisu w tle (Foreground Service)
+        createNotification()
 
         startLocationUpdates()
     }
@@ -49,24 +63,23 @@ class LocationService: Service() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // Wysyłamy broadcast do MainActivity, żeby poprosiła o uprawnienia
+            // Broadcast do MainActivity o potrzebie nadania uprawnień
             val intent = Intent("pl.smolisoft.mapka.REQUEST_LOCATION_PERMISSION")
             sendBroadcast(intent)
+            return
         }
-        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+
+        // Ustawienie częstotliwości aktualizacji na 1 sekundę i minimalnej odległości na 0 metrów
+        locationManager.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1000L, // 1 sekunda
+            5f,    // 0 metrów
+            locationListener
+        )
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        createNotification()
-        return START_STICKY
-    }
-
-    // Tworzenie powiadomienia dla serwisu w tle (wymagane dla Foreground Service)
     @SuppressLint("ForegroundServiceType")
     private fun createNotification() {
         val notificationChannelId = "LOCATION_CHANNEL"
@@ -87,13 +100,13 @@ class LocationService: Service() {
         startForeground(1, notification)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        stopLocationUpdates()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_STICKY
     }
 
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
+    override fun onDestroy() {
+        super.onDestroy()
+        locationManager.removeUpdates(locationListener)
     }
 
     override fun onBind(intent: Intent?): IBinder? {
