0a52d42866381ba1caf944eba521ea36097e6c94
mateuszpob <mateuszpob@gmail.com>
2024-11-05 23:22:29 +0100

    v0.4 - use FusedLocationProviderClient for location cold start

--- app/src/main/java/pl/smolisoft/mapka/services/LocationService.kt
+++ app/src/main/java/pl/smolisoft/mapka/services/LocationService.kt
@@ -17,15 +17,20 @@ import android.os.IBinder
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import pl.smolisoft.mapka.R
 
 class LocationService : Service() {
 
     private lateinit var locationManager: LocationManager
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var isInitialLocationSet = false
+
     private val locationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
-            Log.d("LocationService", "LocationService - Location: ${location.latitude}, ${location.longitude}")
-            LocationRepository.updateLocation(location) // Aktualizacja lokalizacji w repozytorium
+            Log.d("LocationService", "LocationManager - Location: ${location.latitude}, ${location.longitude}")
+            LocationRepository.updateLocation(location)
         }
 
         override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
@@ -44,6 +49,7 @@ class LocationService : Service() {
     override fun onCreate() {
         super.onCreate()
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Tworzenie powiadomienia dla serwisu w tle (Foreground Service)
         createNotification()
@@ -51,23 +57,56 @@ class LocationService : Service() {
         startLocationUpdates()
     }
 
-    private fun startLocationUpdates() {
+    private fun getInitialLocation() {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // Broadcast do MainActivity o potrzebie nadania uprawnień
             val intent = Intent("pl.smolisoft.mapka.REQUEST_LOCATION_PERMISSION")
             sendBroadcast(intent)
             return
         }
 
-        // Ustawienie częstotliwości aktualizacji na 1 sekundę i minimalnej odległości na 0 metrów
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            location?.let {
+                Log.d("LocationService", "FusedLocationClient - Initial Location: ${it.latitude}, ${it.longitude}")
+                LocationRepository.updateLocation(it)
+
+                // Po uzyskaniu początkowej lokalizacji, przełączenie na LocationManager
+                isInitialLocationSet = true
+                startLocationUpdates()
+            } ?: startLocationUpdates() // Jeśli brak lokalizacji, przejście od razu na LocationManager
+        }
+    }
+
+    private fun startLocationUpdates() {
+        if (!isInitialLocationSet) {
+            getInitialLocation()
+            return
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
             1000L, // 1 sekunda
-            5f,    // 0 metrów
+            0f,    // 0 metrów
             locationListener
         )
     }
